00fbf1d064d0856771edf6b4dd79e7030de7ab6b
tarikzircanin <tarikzircanin02@gmail.com>
2026-06-15 21:22:09 +0200

    dodata autentifikacija

---
+++ backend_root/backend/api/authentication.py
@@ -0,0 +1,34 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.authentication import CSRFCheck
+from rest_framework import exceptions
+
+class CookieJWTAuthentication(JWTAuthentication):
+    def authenticate(self, request):
+        header = self.get_header(request)
+        if header is None:
+            raw_token = request.COOKIES.get("access_token")
+        else:
+            raw_token = self.get_raw_token(header)
+
+        if raw_token is None:
+            return None
+        
+        validated_token = self.get_validated_token(raw_token)
+        return self.get_user(validated_token), validated_token
+
+class CsrfExemptJWTAuthentication(JWTAuthentication):
+    def enforce_csrf(self, request):
+        return
+    def authenticate(self, request):
+        header = self.get_header(request)
+        if header is None:
+            raw_token = request.COOKIES.get('access_token')
+        else:
+            raw_token = self.get_raw_token(header)
+
+        if raw_token is None:
+            return None
+
+        validated_token = self.get_validated_token(raw_token)
+        user = self.get_user(validated_token)
+        return (user, validated_token)
